0926dfc8b5ce233dec5c10930675c974eb0bd42d
Elijah Rogers <119324352+jusssEli@users.noreply.github.com>
2025-11-29 17:00:24 -0500

    Update Project.R

--- Project.R
+++ Project.R
@@ -1,1 +1,3 @@
-
+install.packages("RKaggle")  #Library where our dataset is located
+library(RKaggle)
+dataset = get_dataset("mohithsairamreddy/salary-data")  #retrieving/downloading our specific dataset we wanted to work with
